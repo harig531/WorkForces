52b6dad30d2404a6221219a8e2a58f6bef308f91
hguntu@agility.com <hguntu@agility.com>
2021-02-23 22:41:51 +0530

    Add Project Strucutre and Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EmployeeListComponent } from './modules/employee/employee-list/employee-list.component';
-import { EmployeeDetailsComponent } from './modules/employee/employee-details/employee-details.component';
-import { EmployeeFormComponent } from './modules/employee/components/employee-form/employee-form.component';
+import { EmployeeListComponent } from './modules/employee/components/employee-list/employee-list.component';
+import { EmployeeDetailsComponent } from './modules/employee/components/employee-details/employee-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeListComponent,
-    EmployeeDetailsComponent,
-    EmployeeFormComponent
+    EmployeeDetailsComponent
   ],
   imports: [
     BrowserModule,
