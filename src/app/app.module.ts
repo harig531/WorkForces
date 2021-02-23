dd8798c74a8cf131336b57af3b119b6f1c0ce065
hguntu@agility.com <hguntu@agility.com>
2021-02-23 22:40:26 +0530

    add Project Structure and Basic Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeListComponent } from './modules/employee/employee-list/employee-list.component';
+import { EmployeeDetailsComponent } from './modules/employee/employee-details/employee-details.component';
+import { EmployeeFormComponent } from './modules/employee/components/employee-form/employee-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeListComponent,
+    EmployeeDetailsComponent,
+    EmployeeFormComponent
   ],
   imports: [
     BrowserModule,
