a097840e4d50711f147f7330fe92622e659884f3
hguntu@agility.com <hguntu@agility.com>
2021-02-24 12:35:34 +0530

    added Services For Employee List

--- src/app/modules/employee/components/employee-list/employee-list.component.ts
+++ src/app/modules/employee/components/employee-list/employee-list.component.ts
@@ -1,17 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import { EmployeeModel  } from '../../models/Employee'
+import { EmployeeServiceService } from '../../shared/employee-service.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
   styleUrls: ['./employee-list.component.css']
 })
 export class EmployeeListComponent implements OnInit {
+  //_listFilterBy: string;
+  allEmployees?: EmployeeModel[];
+  filteredList?: EmployeeModel[];
+  constructor(private employeeService:EmployeeServiceService, private router: Router) {
 
-  constructor() { }
 
-  ngOnInit(): void {
   }
 
+
+  ngOnInit() {
+
+    this.allEmployees = this.employeeService.listAllEmployees();
+    this.filteredList = this.allEmployees;
+
+  }
 
 
 }
--- src/app/modules/employee/shared/employee-service.service.ts
+++ src/app/modules/employee/shared/employee-service.service.ts
@@ -1,9 +1,55 @@
 import { Injectable } from '@angular/core';
+// import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+// import { HttpClient } from "@angular/common/http";
+import { EmployeeModel } from '../models/Employee'
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeServiceService {
+  allEmployees:EmployeeModel[] = [
+    {
+      "id": 1,
+      "first_name": "Lalit",
+      "last_name": "Aggarwal",
+      "email": "harig531@gmail.com",
+      "avatar": "assets/img/noimage.png"
+    },
+    {
+      "id": 2,
+      "first_name": "Lalit",
+      "last_name": "Aggarwal",
+      "email": "harig531@gmail.com",
+      "avatar": "assets/img/noimage.png"
+    }
+  ];
 
-  constructor() { }
+  listAllEmployees():EmployeeModel[]{
+    return this.allEmployees;
+  }
+
+  addEmployee(employee:EmployeeModel){
+    this.allEmployees.push(employee);
+  }
+
+    // // Update employee details
+    // updateEmployee(employee:EmployeeModel){
+    //   var updateEmployees = this.allEmployees.find(emp => emp.id == employee.id);
+    //   updateEmployees.first_name = employee.first_name;
+    //   updateEmployees.last_name = employee.last_name;
+    //   updateEmployees.email = employee.email;
+    //   updateEmployees.avatar = employee.avatar;
+    // }
+
+
+    deleteEmployee(id:number){
+      this.allEmployees = this.allEmployees.filter(employee => employee.id != id);
+    }
+
+    // // Returns an employee with passed employee id from employee list
+    // getEmployee(id:number):EmployeeModel{
+    //   return this.allEmployees.find(emp => emp.id == id);
+    // }
+
 }
