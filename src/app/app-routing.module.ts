12089b2236ab46c43df1471adc2d44aa68753bf6
hguntu@agility.com <hguntu@agility.com>
2021-02-24 08:27:30 +0530

    Added menu Items card

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EmployeeDetailsComponent } from './modules/employee/components/employee-details/employee-details.component';
+import { EmployeeFormComponent } from './modules/employee/components/employee-form/employee-form.component';
+import { EmployeeListComponent } from './modules/employee/components/employee-list/employee-list.component';
 
-const routes: Routes = [];
+
+const  routes:Routes
+ = [
+    {path : 'employeelist',component:EmployeeListComponent},
+    {path : 'employeedetails/:Id',component:EmployeeDetailsComponent},
+    {path : 'employeeform',component:EmployeeFormComponent},
+    {path : '*',component:EmployeeListComponent}
+
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
+
 export class AppRoutingModule { }
