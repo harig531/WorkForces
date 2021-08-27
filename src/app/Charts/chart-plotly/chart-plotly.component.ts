ad4a5f2305c312079c44945594a8f383d36176eb
Hari Guntu <Hari.Guntu@derivco.co.za>
2021-08-27 13:37:49 +0530

    Pltoy changes

--- src/app/Charts/chart-plotly/chart-plotly.component.ts
+++ src/app/Charts/chart-plotly/chart-plotly.component.ts
@@ -12,12 +12,58 @@ export class ChartPlotlyComponent implements OnInit {
         { x: [1, 2, 3], y: [2, 6, 3], type: 'scatter', mode: 'lines+points', marker: {color: 'red'} },
         { x: [1, 2, 3], y: [2, 5, 3], type: 'bar' },
     ],
-    layout: {width: 320, height: 240, title: 'A Fancy Plot'}
+    layout: {autosize: true, title: 'A Fancy Plot'}
 };
-  constructor() { }
+
+public graph2 = {
+  data: [
+    {
+      y: [8, 19, 20, 22],
+      mode: 'lines+markers',
+      name: 'AvgResponseTime',
+    },
+    {    
+      y: [10, 15, 13, 17],
+      mode: 'lines+markers',
+      name: 'FailedPerSecond',
+    },
+    {
+      y: [12, 9, 15, 12],
+      mode: 'lines+markers',
+      name: 'SuccessPerSecond',
+    }
+  ],
+  layout: {autosize: true, title: 'MARKTING API'},
+};
+
+public graph3 = {
+  data: [{  y: [2, 5, 3], type: 'bar' }],
+  layout: {autosize: true, title: 'A Fancy Plot'},
+};
+
+  constructor() {
+    
+   }
+  
+  //  pData: [];
+  //  pLayout;
 
   ngOnInit(): void {
-   
+    var trace1 = {    
+      y: [10, 15, 13, 17],
+      mode: 'markers'
+    };
+    
+    var trace2 = {
+      y: [16, 5, 11, 9],
+      mode: 'lines'
+    };
+    
+    var trace3 = {
+      y: [12, 9, 15, 12],
+      mode: 'lines+markers'
+    };
+   /// this.pData=[trace1,trace2,trace3]
   }
 
 }
